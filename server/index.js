86492dad62a4ec46d686e98d01c02e53731eff8d
Lehieu06122020 <hmlt22894@gmail.com>
2022-10-31 13:24:13 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const PORT = 4000;
+//New imports
+const http = require('http');
+const io = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+
+const socketIO = io(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+    }
+});
+
+// Conection to socket.io
+let users = [];
+// socketIO.set('transports', ['websocket', 'polling']);
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    // Listens and logs the message to the console
+    socket.on('message', (data) => {
+        socketIO.emit('messageResponse', data);
+    });
+    // Listens when someone is typing something
+    socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+    //Listens when a new user joins the server
+    socket.on('newUser', (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
+  });
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+      //Updates the list of users when a user disconnects from the server
+        users = users.filter((user) => user.socketID !== socket.id);
+        // console.log(users);
+        //Sends the list of users to the client
+        socketIO.emit('newUserResponse', users);
+        socket.disconnect();
+        });
+});
+// endpoint
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'Hello World',
+    });
+});
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
